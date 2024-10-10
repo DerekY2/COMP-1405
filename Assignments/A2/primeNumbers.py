b1f4f68e1fa4ddacf878b636bf36fbe433449f3f
DerekY2 <derek.yu869@gmail.com>
2024-10-09 22:19:10 -0400

    A2 Release

--- Assignments/A2/primeNumbers.py
+++ Assignments/A2/primeNumbers.py
@@ -5,11 +5,17 @@
 '''
 
 import random
-import divisors
+import divisors # import function findDivisors()
 
+#  generate a number between 1-30(inclusive)
 number = random.randint(1,30)
 print(f"The generated number is: {number}")
+
+# if findDivisors(number) returns 2 divisors, they must
+# be 1 and the number itself, therefore "number" is a prime number.
+# However 1 isn't a prime number.
 if number!=1 and len(divisors.findDivisors(number))<3:
   print("This is a prime number!")
+# more than 2 divisors, so it is not a prime number
 else:
   print("This is not a prime number.")
