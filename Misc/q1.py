d3d0eef963c1e27af955b6353e06f68e6aeece64
DerekY2 <derek.yu869@gmail.com>
2024-10-11 19:42:24 -0400

    Misc added

---
+++ Misc/q1.py
@@ -0,0 +1,37 @@
+i = 18
+b = True
+
+while b:
+  print(i)
+  i+=1
+  if i>98:
+    b=False
+
+while b:
+  print(i)
+  i+=1
+  if i>98:
+    break
+
+thing = 3.134534
+print(f"{thing: .4f}")
+print(f"{round(thing,4)}")
+
+def ask(q):
+  return input(q).upper()
+# if ask("Does \"this\" work: ") == "Y":
+#   print("It indeed works")
+# else:
+#   print("No workey :(")
+
+
+if ask("Is it 1")=="Y":
+  print("Yes it is 1")
+elif ask("Is it 2")=="Y":
+  print("Yes it is 2")
+elif ask("Is it 3")=="Y":
+  print("Yes it is 3")
+elif ask("Is it 4")=="Y":
+  print("Yes it is 4")
+else:
+  print("It is 5")
